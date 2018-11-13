d6d615cee3e1901e511443b322695e577887e2e2
Vishwas Navada K <navadavishwas@hotmail.com>
2018-11-13 11:35:09 +0530

    fixed reducer thing

--- todolist/src/reducers/todo_reducer.js
+++ todolist/src/reducers/todo_reducer.js
@@ -1,10 +1,11 @@
-import FETCH_TODOLIST from './index';
+import _ from 'lodash';
+
+import {FETCH_TODOLIST} from '../actions/index';
 
 export default function(state={},action) {
     switch(action.type){ 
         case FETCH_TODOLIST:
-            console.log(action.payload);
-            return action.payload;
+            return _.keyBy(action.payload,"id");
         default:
             return state;
     }
