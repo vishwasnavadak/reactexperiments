0d1b5693d1bc4115e503cd5c9bfb370a1991d783
Vishwas Navada K <navadavishwas@hotmail.com>
2018-11-15 16:52:53 +0530

    updated

--- todolist/src/reducers/todo_reducer.js
+++ todolist/src/reducers/todo_reducer.js
@@ -1,11 +1,30 @@
 import _ from 'lodash';
 
-import {FETCH_TODOLIST} from '../actions/index';
+import {ActionTypes} from '../actions/index';
 
 export default function(state={},action) {
     switch(action.type){ 
-        case FETCH_TODOLIST:
-            return _.mapKeys(action.payload,"id");
+        case ActionTypes.FETCH_TODOLIST:
+            return _.mapKeys(_.orderBy(action.payload,["isCompleted","timestamp"],["asc","desc"]),"id");
+        case ActionTypes.ADD_TODO:
+            //TODO adding the new element to state
+           /*  console.log(action.payload);
+            console.log(state);
+            return ({...state,action.payload['id']:{}}); */
+            return state;
+        case ActionTypes.DELETE_TODO:
+            return _.omit(state,action.payload);
+        case ActionTypes.UPDATE_TODO:
+            const newState = _.map(state,item => {
+                if(item.id === action.payload) {
+                   return {
+                       ...item, 
+                       isCompleted:!item.isCompleted
+                   }
+                }
+                return item;
+            })
+            return _.orderBy(newState,["isCompleted","timestamp"],["asc","desc"]);
         default:
             return state;
     }
