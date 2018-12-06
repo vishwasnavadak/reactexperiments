f778d326ad59bd0a6fb0f8971bd2b3821478c808
Vishwas Navada <navadavishwas@hotmail.com>
2018-12-06 22:07:43 +0530

    refactored the code with new API end points

--- todolist/src/actions/index.js
+++ todolist/src/actions/index.js
@@ -2,66 +2,45 @@ export const ActionTypes = {FETCH_TODOLIST:"FETCH_TODOLIST",
                             ADD_TODO:"ADD_TODO",
                             DELETE_TODO:"DELETE_TODO",
                             UPDATE_TODO:"UPDATE_TODO"};
+                            
+const API_URL = "https://apis.vishwas.tech/todolist/";
+const API_KEY = "0j236i3CIl3lULyU98ZXK2zRpbQfN9Fc3Bu0kVn40PK";
 
-export function fetchTodoList(){
-    var request = fetch('https://xzm0v749a7.execute-api.us-east-1.amazonaws.com/default/todoFetchItems',{
-        method: 'GET',
+function apiCall(apiEndpoint,apiMethod){
+    return fetch(`${API_URL}${apiEndpoint}`,{
+        method: apiMethod,
         headers:{
             'Content-Type': 'application/json',
-            'x-api-key': '7BAzKhsJHd80OKCUErTSl5jtq9L4lanY6za8dkVnpKX'
+            'x-api-key': API_KEY
         }
     })
     .then(response => response.json())
     .then((data) => { return data; } )
-    .catch(error => console.log('Error while fetching:', error));
+    .catch(error => console.log('Error in API Call:', error));
+}
+export function fetchTodoList(){
+    var request = apiCall("todoFetchItems","GET")
     return ({
         type:ActionTypes.FETCH_TODOLIST,
         payload:request
     })
 }
-export function addTodo(item,callback){
-    var request = fetch(`https://pp0qmqrqt6.execute-api.us-east-1.amazonaws.com/default/todoAddItem?desc=${item}`,{
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json',
-            'x-api-key': 'FBbnY5sYVG6amtR8Fcz8t8bLS5epGKMN3gRMMkVnIuv'
-        }
-    })
-    .then(response => response.json())
-    .then((data) => { return data; } )
-    .catch(error => console.log('Error while adding:', error));
+export function addTodo(item){
+    var request = apiCall(`todoAddItem?desc=${item}`,"POST")
     return ({
         type:ActionTypes.ADD_TODO,
         payload:request
     })
 }
 export function deleteTodo(id){
-    var request = fetch(`https://ps0cknoeeb.execute-api.us-east-1.amazonaws.com/default/todoDeleteItem?id=${id}`,{
-        method: 'DELETE',
-        headers:{
-            'Content-Type': 'application/json',
-            'x-api-key': 'CPTN4x3C8m4jCiInMlwGga2ye9lQOTU3BMfEjkVnLvf'
-        }
-    })
-    .then(response => response.json())
-    .then((data) => { return data; } )
-    .catch(error => console.log('Error while deleting:', error));
+    var request = apiCall(`todoDeleteItem?id=${id}`,"DELETE")
     return ({
         type:ActionTypes.DELETE_TODO,
         payload:request
     })
 }
 export function updateTodo(id){
-    var request = fetch(`https://d7h55xdv0f.execute-api.us-east-1.amazonaws.com/default/todoUpdateItem?id=${id}`,{
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json',
-            'x-api-key': 'MMCCg0M7LX9J0bIPxvarw182EZVUvnaMaNWWZkVnNXa'
-        }
-    })
-    .then(response => response.json())
-    .then((data) => { return data; } )
-    .catch(error => console.log('Error while updating:', error));
+    var request = apiCall(`todoUpdateItem?id=${id}`,"POST")
     return ({
         type:ActionTypes.UPDATE_TODO,
         payload:request
